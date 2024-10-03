46428d47dc22ebcad30668b8cecae78ad91080a3
Vinaya Kumar Bendi <bvinayakumar@outlook.com>
2024-10-03 07:41:07 +0530

    part 1: resolve compilation error

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,19 +1,19 @@
 "use client";
 
 import CardGrid from "@/components/CardGrid";
-import { CardProps } from "@/types/DraggableCardProps";
+import { DraggableCardProps } from "@/types/DraggableCardProps";
 import { useEffect, useState } from "react";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 
 export default function Home() {
-  const [cards, setCards] = useState<CardProps[]>([]);
+  const [cards, setCards] = useState<DraggableCardProps[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch("./data.json");
-        const data: CardProps[] = await response.json();
+        const data: DraggableCardProps[] = await response.json();
         setCards(data.map((card, index) => ({ ...card, id: index })));
       } catch (error) {
         console.error("Error fetching data:", error);
